95978043c6d438466b1367f3e196fe3ac0f14a1c
zeldran04 <50961641+zeldran04@users.noreply.github.com>
2019-06-03 10:49:34 -0700

    Add files via upload

---
+++ SubStrikeAilan/SubStrike/core/src/state/PlayState.java
@@ -0,0 +1,88 @@
+package state;
+
+import java.awt.event.KeyEvent;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.mygdx.game.SubStrike;
+
+import sprites.Fish;
+import sprites.Submarine;
+
+public class PlayState extends State
+{
+	private Submarine sub;
+	private Texture background;
+	private Texture backgroundclouds;
+	private Fish fish;
+	private TextureRegion imgTextureRegion;
+    private int offset = 0;
+    private int CamX = 0;
+    private int CamX1 = -1;
+	public PlayState(GameStateManager gsm) 
+	{
+		super(gsm);
+		sub = new Submarine(50, 200);
+		//cam.setToOrtho(false, SubStrike.WIDTH /2, SubStrike.HEIGHT / 2);
+		
+		
+		backgroundclouds = new Texture("Backgroundclouds1.png");
+		background = new Texture("background1.png");
+       
+		//fish = new Fish(100);
+		
+	}
+
+	@Override
+	public void handleInput() 
+	{
+		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
+		{
+			sub.jump();
+		}
+
+
+	}
+
+	@Override
+	public void update(float dt) 
+	{
+		handleInput();
+		sub.update(dt);
+		CamX -= 5;
+		CamX1 -= 1;
+	
+	}
+
+	@Override
+	public void render(SpriteBatch sb) 
+	{
+		//sb.setProjectionMatrix(cam.combined); //where in the game world we are
+		sb.begin();
+		
+	if (CamX  < -background.getWidth() + 1920  - offset   )
+	   {
+	    offset += background.getWidth() - 1920;
+	    
+	    }
+	
+		sb.draw(backgroundclouds, CamX1  + offset, 0);
+		sb.draw(background, CamX  + offset ,0);
+
+		sb.draw(sub.getTexture(), sub.getPosition().x, sub.getPosition().y);
+		
+		
+				//sb.draw(fish.getFish1(), fish.getPosFish().x, fish.getFish1().y);
+		sb.end();
+	}
+
+	@Override
+	public void dispose() 
+	{
+
+	}
+
+}
